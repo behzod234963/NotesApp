b6870730ca3dff8c01f495c33c236d379787378a
Behzod96447 <behzod234963@internet.ru>
2023-09-25 14:24:10 +0500

    NoteApp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.notesapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.notesapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         viewBinding.enable=true
